f0676622149d18ac0c613a39fb8f5402c2bab647
cjherm <akbigchris@gmail.com>
2026-04-03 21:50:33 +0200

    Shortened the toString output of CopyCatConfiguration.

--- src/main/kotlin/app/CopyCatConfiguration.kt
+++ src/main/kotlin/app/CopyCatConfiguration.kt
@@ -8,17 +8,11 @@ class CopyCatConfiguration(
     val filesSelectedToBeCopied: List<File>,
 ) {
     override fun toString(): String {
-        val fileList = if (filesSelectedToBeCopied.isEmpty()) {
-            "\t\t(none)"
-        } else {
-            filesSelectedToBeCopied.joinToString("\n") { "\t\t${it.absolutePath}" }
-        }
         return """
             |CopyCat Configuration
             |  Source directory   : ${sourceDir.absolutePath}
             |  Destination        : ${copyDestDir.absolutePath}
             |  Files to copy      : ${filesSelectedToBeCopied.size}
-            |$fileList
         """.trimMargin()
     }
 }
