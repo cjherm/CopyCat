a551a24e941909f3dfe2028e22d231dde4d090f1
cjherm <akbigchris@gmail.com>
2026-04-04 09:22:34 +0200

    Added more logic to Logger.

--- src/main/kotlin/utility/Logger.kt
+++ src/main/kotlin/utility/Logger.kt
@@ -1,20 +1,50 @@
 package utility
 
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 object Logger {
 
-    fun info(msg: String) {
-        ConsolePrinter.printWhite("[INFO]  $msg")
+    private const val MAX_BUFFER_SIZE = 1000
+    private val timestampFormatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss")
+
+    var printToConsole = false
+
+    var logFile: File? = null
+        set(value) {
+            field = value
+            flushLogBuffer()
+        }
+
+    private val logBuffer = mutableListOf<String>()
+
+    fun info(msg: String) = log(msg, "[INFO]") { ConsolePrinter.printWhite(it) }
+    fun warn(msg: String) = log(msg, "[WARN]") { ConsolePrinter.printYellow(it) }
+    fun error(msg: String) = log(msg, "[ERROR]") { ConsolePrinter.printRed(it) }
+
+    private fun log(msg: String, prefix: String, consolePrint: (String) -> Unit) {
+        val timestamp = LocalDateTime.now().format(timestampFormatter)
+        val formatted = "$timestamp $prefix $msg"
+        if (printToConsole) consolePrint(formatted)
+        if (logFile != null) {
+            logInFile(formatted)
+            return
+        }
+        if (logBuffer.size >= MAX_BUFFER_SIZE) {
+            logBuffer.removeFirst()
+            logBuffer.add("$timestamp [WARN] Log buffer was full, oldest message was dropped.")
+        }
+        logBuffer.add(formatted)
     }
 
-    fun success(msg: String) {
-        ConsolePrinter.printGreen("[OK]    $msg")
+    private fun flushLogBuffer() {
+        while (logBuffer.isNotEmpty()) {
+            logInFile(logBuffer.removeFirst())
+        }
     }
 
-    fun warn(msg: String) {
-        ConsolePrinter.printYellow("[WARN]  $msg")
-    }
-
-    fun error(msg: String) {
-        ConsolePrinter.printRed("[ERROR] $msg")
+    private fun logInFile(msg: String) {
+        logFile?.appendText("$msg\n")
     }
 }
