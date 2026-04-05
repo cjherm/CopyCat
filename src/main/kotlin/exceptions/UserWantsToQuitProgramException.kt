fe7096c19649a70edd1d6a21cf70e780dd3a39fc
cjherm <akbigchris@gmail.com>
2026-04-05 21:27:38 +0200

    Restructuring: Moved UserWantsToQuitProgramException.

--- src/main/kotlin/exceptions/UserWantsToQuitProgramException.kt
+++
@@ -1,5 +0,0 @@
-package exceptions
-
-class UserWantsToQuitProgramException: Throwable() {
-    // empty on purpose
-}
---
+++ src/main/kotlin/utility/UserWantsToQuitProgramException.kt
@@ -0,0 +1,5 @@
+package utility
+
+class UserWantsToQuitProgramException : Throwable() {
+    // empty on purpose
+}
