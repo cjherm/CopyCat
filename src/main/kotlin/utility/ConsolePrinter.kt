d3507c5dd526c09f42650c63e466a8b71820dca2
cjherm <akbigchris@gmail.com>
2026-06-14 19:49:07 +0200

    Removed redundant coloring.

--- src/main/kotlin/utility/ConsolePrinter.kt
+++ src/main/kotlin/utility/ConsolePrinter.kt
@@ -7,9 +7,6 @@ class ConsolePrinter {
         const val RED = "\u001B[31m"
         const val GREEN = "\u001B[32m"
         const val YELLOW = "\u001B[33m"
-        const val BLUE = "\u001B[34m"
-        const val PURPLE = "\u001B[35m"
-        const val CYAN = "\u001B[36m"
         const val WHITE = "\u001B[37m"
     }
 
@@ -28,18 +25,6 @@ class ConsolePrinter {
 
         fun printYellow(msg: String) {
             printColor(msg, AnsiColor.YELLOW)
-        }
-
-        fun printBlue(msg: String) {
-            printColor(msg, AnsiColor.BLUE)
-        }
-
-        fun printPurple(msg: String) {
-            printColor(msg, AnsiColor.PURPLE)
-        }
-
-        fun printCyan(msg: String) {
-            printColor(msg, AnsiColor.CYAN)
         }
 
         fun printWhite(msg: String) {
