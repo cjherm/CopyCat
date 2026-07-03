0e68978dae21052dd6edd027bf91401eb2b38bbe
cjherm <akbigchris@gmail.com>
2026-07-03 20:20:15 +0200

    Multi-commit: Expanded src and dest config info to multiple entries.

--- src/main/kotlin/config/CopyCatConfiguration.kt
+++ src/main/kotlin/config/CopyCatConfiguration.kt
@@ -3,18 +3,20 @@ package config
 import java.io.File
 
 class CopyCatConfiguration(
-    val sourceDir: File,
-    val copyDestDir: File,
+    val sourceDirs: List<File>,
+    val copyDestDirs: List<File>,
     val filesSelectedToBeCopied: List<File>,
     val printToConsole: Boolean,
     val printToFile: Boolean
 ) {
     override fun toString(): String {
+        val srcPaths = sourceDirs.joinToString("\n\t\t\t\t\t\t\t\t") { it.absolutePath }
+        val destPaths = copyDestDirs.joinToString("\n\t\t\t\t\t\t\t\t") { it.absolutePath }
         return "CopyCat Configuration:\n" +
-                "                            \tSource directory: ${sourceDir.absolutePath}\n" +
-                "                            \tDestination:      ${copyDestDir.absolutePath}\n" +
-                "                            \tFiles to copy:    ${filesSelectedToBeCopied.size}" +
-                "                            \tprintToConsole:   $printToConsole\n" +
-                "                            \tprintToFile:      $printToFile\n"
+                "                            \tSource directories: $srcPaths\n" +
+                "                            \tDestinations:       $destPaths\n" +
+                "                            \tFiles to copy:      ${filesSelectedToBeCopied.size}\n" +
+                "                            \tprintToConsole:     $printToConsole\n" +
+                "                            \tprintToFile:        $printToFile\n"
     }
 }
