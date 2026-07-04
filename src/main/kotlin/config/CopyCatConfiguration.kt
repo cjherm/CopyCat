6bdf45d2b669f2a7bfc4d3abc2a8192766a6bd8d
cjherm <akbigchris@gmail.com>
2026-07-04 11:03:53 +0200

    Adjusted cfg print out.

--- src/main/kotlin/config/CopyCatConfiguration.kt
+++ src/main/kotlin/config/CopyCatConfiguration.kt
@@ -1,5 +1,6 @@
 package config
 
+import utility.Logger
 import java.io.File
 
 class CopyCatConfiguration(
@@ -12,11 +13,13 @@ class CopyCatConfiguration(
     override fun toString(): String {
         val srcPaths = sourceDirs.joinToString("\n\t\t\t\t\t\t\t\t") { it.absolutePath }
         val destPaths = copyDestDirs.joinToString("\n\t\t\t\t\t\t\t\t") { it.absolutePath }
+        val logFilePath = Logger.logFile?.absolutePath ?: "none"
         return "CopyCat Configuration:\n" +
                 "                            \tSource directories: $srcPaths\n" +
                 "                            \tDestinations:       $destPaths\n" +
                 "                            \tFiles to copy:      ${filesSelectedToBeCopied.size}\n" +
                 "                            \tlogToConsole:       $printToConsole\n" +
-                "                            \tlogToFile:          $printToFile\n"
+                "                            \tlogToFile:          $printToFile\n" +
+                "                            \tlogFile:            $logFilePath"
     }
 }
